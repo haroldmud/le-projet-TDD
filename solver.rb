85d3558471128d98512439b634817876f772f31e
haroldmud <haroldmudosa@gmail.com>
2022-09-03 08:51:33 +0200

    Created fizzbuzz conditions

--- solver.rb
+++ solver.rb
@@ -14,4 +14,16 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(int)
+    if (int % 3).zero? && (int % 5).zero?
+      'fizzbuzz'
+    elsif (int % 3).zero?
+      'fizz'
+    elsif (int % 5).zero?
+      'buzz'
+    else
+      int.to_s
+    end
+  end
 end
