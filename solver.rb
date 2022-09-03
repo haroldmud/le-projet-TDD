a36bb97e5b135a1a1974291ef2887c9db5e82d90
haroldmud <haroldmudosa@gmail.com>
2022-09-03 08:39:06 +0200

    Implement the factorial function

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
 class Solver
+  def initialize; end
 
+  def factorial(int)
+    if int.zero?
+      1
+    elsif int.negative?
+      puts 'Math error!'
+    else
+      int * factorial(int - 1)
+    end
+  end
 end
